3bbd3f35ad80872fd32a2062ea8d05164c4ad71d
Hatano Yumi <>
2025-07-02 17:47:13 +0900

    複数ページテスト

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,24 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @use "./src/styles/_variables.scss" as *;
+          @use "./src/styles/_mixin.scss" as *;
+          `,
+        }
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: "[ext]/[name][extname]",// cssの名前をわかりやすく
+        },
+      },
+    },
+  },
+});
